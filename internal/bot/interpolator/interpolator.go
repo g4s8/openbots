33203e87adda9f5ae8cba828f010283cc44e8c1f
Kirill <g4s8.public@gmail.com>
2023-08-23 18:43:09 +0400

    feat: interpolator chat and user data

--- internal/bot/interpolator/interpolator.go
+++ internal/bot/interpolator/interpolator.go
@@ -12,40 +12,54 @@ import (
 type Interpolator struct {
 	state   map[string]string
 	secrets map[string]types.Secret
-
-	message *telegram.Message
+	upd     *telegram.Update
 }
 
 func New(state map[string]string, secrets map[string]types.Secret, upd *telegram.Update) *Interpolator {
-	res := &Interpolator{
+	return &Interpolator{
 		state:   state,
 		secrets: secrets,
+		upd:     upd,
 	}
-	if upd != nil && upd.Message != nil {
-		res.message = upd.Message
-	}
-	return res
 }
 
 func (i *Interpolator) expander() func(string) string {
-	message := make(map[string]string)
-	if i.message != nil {
-		message["id"] = strconv.Itoa(i.message.MessageID)
-		message["text"] = i.message.Text
-		message["from.id"] = strconv.FormatInt(i.message.From.ID, 10)
+	data := make(map[string]string)
+	if upd := i.upd; upd != nil {
+		if msg := upd.Message; msg != nil {
+			data["message.id"] = strconv.Itoa(msg.MessageID)
+			data["message.text"] = msg.Text
+			data["message.from.id"] = strconv.FormatInt(msg.From.ID, 10)
+		}
+		if chat := upd.FromChat(); chat != nil {
+			data["chat.id"] = strconv.FormatInt(chat.ID, 10)
+			data["chat.type"] = chat.Type
+			data["chat.title"] = chat.Title
+			data["chat.first_name"] = chat.FirstName
+			data["chat.last_name"] = chat.LastName
+			data["chat.username"] = chat.UserName
+		}
+		if user := upd.SentFrom(); user != nil {
+			data["user.id"] = strconv.FormatInt(user.ID, 10)
+			data["user.is_bot"] = strconv.FormatBool(user.IsBot)
+			data["user.first_name"] = user.FirstName
+			data["user.last_name"] = user.LastName
+			data["user.username"] = user.UserName
+			data["user.language_code"] = user.LanguageCode
+		}
 	}
 	return func(text string) string {
 		if strings.HasPrefix(text, "state.") {
 			return i.state[text[6:]]
-		}
-		if strings.HasPrefix(text, "message.") {
-			return message[text[8:]]
 		}
 		if strings.HasPrefix(text, "secret.") {
 			secret, ok := i.secrets[text[7:]]
 			if ok {
 				return secret.Value()
 			}
+		}
+		if val, ok := data[text]; ok {
+			return val
 		}
 		return ""
 	}
